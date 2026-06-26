c3efc4d625804dad895b672808e5e14ba99cbbf5
DreikYzat <dreik013.dcd@gmail.com>
2026-06-26 15:19:58 -0300

    Publica portfólio YZAT

---
+++ js/script.js
@@ -0,0 +1,79 @@
+const canvas = document.getElementById("matrixCanvas");
+    const ctx = canvas.getContext("2d");
+
+    function ajustarCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
+
+    ajustarCanvas();
+
+    const letras = "0101010101YZATDEVAPI";
+    const fonte = 16;
+    let colunas = Math.floor(canvas.width / fonte);
+    let gotas = [];
+
+    function iniciarGotas() {
+      colunas = Math.floor(canvas.width / fonte);
+      gotas = [];
+      for (let i = 0; i < colunas; i++) {
+        gotas[i] = Math.random() * canvas.height;
+      }
+    }
+
+    iniciarGotas();
+
+    function desenharMatrix() {
+      ctx.fillStyle = "rgba(0, 0, 0, 0.08)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      ctx.fillStyle = "#00ff99";
+      ctx.font = fonte + "px monospace";
+
+      for (let i = 0; i < gotas.length; i++) {
+        const texto = letras[Math.floor(Math.random() * letras.length)];
+        ctx.fillText(texto, i * fonte, gotas[i] * fonte);
+
+        if (gotas[i] * fonte > canvas.height && Math.random() > 0.975) {
+          gotas[i] = 0;
+        }
+
+        gotas[i]++;
+      }
+    }
+
+    setInterval(desenharMatrix, 45);
+
+    window.addEventListener("resize", () => {
+      ajustarCanvas();
+      iniciarGotas();
+    });
+
+    const linhasTerminal = [
+      "> Inicializando YZAT...",
+      "✔ HTML carregado",
+      "✔ CSS aplicado",
+      "✔ JavaScript executado",
+      "✔ APIs conectadas",
+      "✔ Banco de Dados online",
+      "✔ Bem-vindo ao meu laboratório."
+    ];
+
+    let linhaAtual = 0;
+    let textoFinal = "";
+
+    function escreverTerminal() {
+      const terminal = document.getElementById("terminalText");
+
+      if (!terminal) return;
+
+      if (linhaAtual < linhasTerminal.length) {
+        textoFinal += linhasTerminal[linhaAtual] + "\n";
+        terminal.innerHTML = textoFinal + '<span class="cursor">█</span>';
+        linhaAtual++;
+
+        setTimeout(escreverTerminal, 650);
+      }
+    }
+
+    window.onload = escreverTerminal;
